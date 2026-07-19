ea98179f3e1b5b2a872ccc92e32df0750fc5075e
manyia34 <mahajanmanyia790@gmail.com>
2026-07-20 00:56:15 +0530

    Authentication

--- security.py
+++ security.py
@@ -1,9 +1,9 @@
 from pwdlib import PasswordHash
 password_hash  = PasswordHash.recommended()
 
-def hash_password(password):
+def hash_password(password : str) -> str:
     return password_hash.hash(password)
 
-def verify_password(original_password,hash_password):
+def verify_password(original_password : str ,hash_password : str) -> str :
     return password_hash.verify(original_password,hash_password)
 
